5833390a8b85736a2b69f2bc6f1c16d48534dbbb
nickhsine <nickhsine97753017@gmail.com>
2019-05-25 16:43:01 +0800

    Update aside:metadata. Use dynamic-components-context to get Link component and add hover effect

--- src/components/aside/metadata.js
+++ src/components/aside/metadata.js
@@ -1,3 +1,4 @@
+import DynamicComponentsContext from '../../contexts/dynamic-components-context'
 import React, { PureComponent } from 'react'
 import map from 'lodash/map'
 import mq from '../../utils/media-query'
@@ -28,7 +29,7 @@ const createLine = topOrBottom => {
   return css`
     position: relative;
     border-${topOrBottom}: solid 0.5px #d8d8d8;
-    padding-${topOrBottom}: 15px;
+    padding-${topOrBottom}: 10px;
 
     &::after {
       content: '';
@@ -62,10 +63,16 @@ const CategoryFlex = styled.div`
 `
 
 const CategoryText = styled.div`
+  display: inline-block;
   color: ${props => props.theme.colors.primary.text};
   font-size: ${props => props.theme.fontSizeOffset + 16}px;
   line-height: 1;
   padding-left: 5px;
+
+  &:hover {
+    padding-bottom: 2px;
+    border-bottom: 1px solid ${props => props.theme.colors.primary.text};
+  }
 `
 
 const DateSection = styled.div`
@@ -107,6 +114,12 @@ const AuthorName = styled(AuthorJobTitle)`
   color: ${props => props.theme.colors.primary.text};
   font-size: ${props => props.theme.fontSizeOffset + 16}px;
   margin-left: 5px;
+  padding-bottom: 3px;
+
+  &:hover {
+    padding-bottom: 2px;
+    border-bottom: 1px solid ${props => props.theme.colors.primary.text};
+  }
 
   ${mq.mobileOnly`
     display: inline-block;
@@ -152,10 +165,6 @@ const TagsSection = styled.div`
   ${createLine('bottom')}
 `
 
-const StyledA = styled.a`
-  text-decoration: none;
-`
-
 class Metadata extends PureComponent {
   static propTypes = predefinedProps.metadata
 
@@ -174,38 +183,47 @@ class Metadata extends PureComponent {
 
     _.sortBy(categories, ['sort_order'])
 
-    const categoriesJSX = _.map(categories, (cat, index) => {
-      return (
-        <CategoryFlex key={`category_${cat.id}`} flexGrow={index}>
-          <StyledA href={`/categories/${cat.id}`}>
-            <CategoryText
-              style={{ fontWeight: index === 0 ? 'bold' : 'normal' }}
-            >
-              {cat.name}
-            </CategoryText>
-          </StyledA>
-        </CategoryFlex>
-      )
-    })
-
-    return <CategoryFlexBox>{categoriesJSX}</CategoryFlexBox>
+    return (
+      <CategoryFlexBox>
+        <DynamicComponentsContext.Consumer>
+          {components => {
+            const categoriesJSX = _.map(categories, (cat, index) => {
+              return (
+                <CategoryFlex key={`category_${cat.id}`} flexGrow={index}>
+                  <components.Link to={`/categories/${cat.id}`}>
+                    <CategoryText
+                      style={{ fontWeight: index === 0 ? 'bold' : 'normal' }}
+                    >
+                      {cat.name}
+                    </CategoryText>
+                  </components.Link>
+                </CategoryFlex>
+              )
+            })
+            return categoriesJSX
+          }}
+        </DynamicComponentsContext.Consumer>
+      </CategoryFlexBox>
+    )
   }
 
   renderTagsSection() {
     const { tags } = this.props
 
-    const tagsJSX = _.map(tags, tag => {
-      return (
-        <StyledA key={`tag_${tag.id}`} href={`/tags/${tag.id}`}>
-          <TagButton>{tag.name}</TagButton>
-        </StyledA>
-      )
-    })
-
     return (
-      <React.Fragment>
-        <TagsSection>{tagsJSX}</TagsSection>
-      </React.Fragment>
+      <TagsSection>
+        <DynamicComponentsContext.Consumer>
+          {components => {
+            return _.map(tags, tag => {
+              return (
+                <components.Link key={`tag_${tag.id}`} to={`/tags/${tag.id}`}>
+                  <TagButton>{tag.name}</TagButton>
+                </components.Link>
+              )
+            })
+          }}
+        </DynamicComponentsContext.Consumer>
+      </TagsSection>
     )
   }
 
@@ -213,14 +231,6 @@ class Metadata extends PureComponent {
     if (authors.length === 0) {
       return null
     }
-
-    const authorNamesJSX = _.map(authors, author => {
-      return (
-        <StyledA key={`author_${author.id}`} href={`/authors/${author.id}`}>
-          <AuthorName>{author.name}</AuthorName>
-        </StyledA>
-      )
-    })
 
     return (
       <AuthorFlexBox>
@@ -230,7 +240,20 @@ class Metadata extends PureComponent {
         <NoShrinkFlexItem>
           <AngledSeparationLine />
         </NoShrinkFlexItem>
-        <div>{authorNamesJSX}</div>
+        <DynamicComponentsContext.Consumer>
+          {components => {
+            return _.map(authors, author => {
+              return (
+                <components.Link
+                  key={`author_${author.id}`}
+                  to={`/authors/${author.id}`}
+                >
+                  <AuthorName>{author.name}</AuthorName>
+                </components.Link>
+              )
+            })
+          }}
+        </DynamicComponentsContext.Consumer>
       </AuthorFlexBox>
     )
   }
